235514da951eedd9791b0eb9287dccc2c1895e47
stdthoth <ichthoth@gmail.com>
2025-01-06 06:38:35 +0100

    output length

--- inference_workflow.py
+++ inference_workflow.py
@@ -8,7 +8,7 @@ model = GPT2LMHeadModel.from_pretrained("./fine_tuned_gpt2")
 input_text = "Who is Gengis Khan"
 input_ids = tokenizer.encode(input_text, return_tensors="pt")
 
-output = model.generate(input_ids, max_length=50, num_return_sequences=1)
+output = model.generate(input_ids, max_length=300, num_return_sequences=1)
 print(tokenizer.decode(output[0], skip_special_tokens=True))
 
 
